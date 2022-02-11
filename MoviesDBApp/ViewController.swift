6f30c70001e6817f7e64aa88939b3dad8982aa3a
Thomas <unitide@gmail.com>
2022-02-11 13:31:42 -0500

    Initial Commit

---
+++ MoviesDBApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesDBApp
+//
+//  Created by Mingyong Zhu on 2/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
